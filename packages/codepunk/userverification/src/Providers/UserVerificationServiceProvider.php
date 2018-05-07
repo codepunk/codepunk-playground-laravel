76cddac121176b40bcd316309c49475fa553ff62
Scott Slater <slaterama@gmail.com>
2018-05-07 11:24:16 -0400

    Set up skeleton for user verification package

---
+++ packages/codepunk/userverification/src/Providers/UserVerificationServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Codepunk\UserVerification\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserVerificationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        //dd('Made it to UserVerificationServiceProvider::boot');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        //dd('Made it to UserVerificationServiceProvider::register');
+    }
+}
